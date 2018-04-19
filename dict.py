b93f652d08c35abfcd5084636d89058e9b302d50
mindhacked <34209551+mdymek@users.noreply.github.com>
2018-04-19 15:52:29 +0200

    Update dict.py
    removed the things (hopefully all)

--- dict.py
+++ dict.py
@@ -1,44 +1,44 @@
 __author__ = 'Meg'
 
-animals = {'alligator', 'ant', 'bear', 'bee', 'bird', 'camel', 'cat', 'cheetah',
+animals = ['alligator', 'ant', 'bear', 'bee', 'bird', 'camel', 'cat', 'cheetah',
            'chicken', 'chimpanzee', 'cow', 'crocodile', 'deer',  'dog',  'dolphin',
            'duck', 'eagle', 'elephant', 'fish', 'fly', 'fox', 'frog', 'giraffe',
-           'goat', 'goldfish', 'hamster', 'hippopotamus', 'horse', 'kangaroo', 'kitten',
+           'goat', 'hamster', 'hippopotamus', 'horse', 'kangaroo',
            'leopard', 'lion', 'lizard', 'lobster', 'monkey', 'octopus', 'ostrich',
            'otter', 'owl', 'oyster', 'panda', 'parrot', 'pelican', 'pig', 'pigeon',
-           'porcupine', 'puppy', 'rabbit', 'rat', 'reindeer' , 'rhinoceros', 'rooster',
+           'porcupine', 'rabbit', 'rat', 'reindeer' , 'rhinoceros', 'rooster',
            'scorpion', 'seal', 'shark', 'sheep', 'shrimp', 'snail', 'snake', 'sparrow',
            'spider', 'squid', 'squirrel', 'swallow', 'swan', 'tiger', 'toad',
-           'tortoise', 'turtle', 'vulture', 'walrus', 'weasel', 'whale', 'wolf', 'zebra'}
+           'tortoise', 'turtle', 'vulture', 'walrus', 'weasel', 'whale', 'wolf', 'zebra']
 
-body_parts = {'leg', 'tongue', 'blood', 'bone', 'stomach', 'chin', 'tooth', 'foot', 'skin',
-              'wrist', 'hand', 'thumb', 'arm', 'feet', 'brain', 'shoulders', 'nail', 'hands',
-              'eye', 'hip', 'thumbs', 'shoulder', 'head', 'eyes', 'beard', 'back', 'toes',
-              'ears', 'moustache', 'knees', 'finger', 'hair', 'throat', 'body', 'ear', 'muscle',
-              'elbow', 'toe', 'cheek', 'legs', 'chest', 'neck', 'face', 'mouth', 'ankle',
-              'nose', 'teeth', 'fingers', 'heart', 'knee', 'flesh', 'lip'}
+body_parts = ['leg', 'tongue', 'blood', 'bone', 'stomach', 'chin', 'tooth', 'foot', 'skin',
+              'wrist', 'hand', 'thumb', 'arm', 'brain', 'nail',
+              'eye', 'hip', 'shoulder', 'head', 'beard', 'back',
+              'moustache', 'finger', 'hair', 'throat', 'body', 'ear', 'muscle',
+              'elbow', 'toe', 'cheek', 'chest', 'neck', 'face', 'mouth', 'ankle',
+              'nose', 'heart', 'knee', 'flesh', 'lip']
 
-buildings = {'cathedral', 'supermarket', 'cafe', 'shrine', 'apartment building', 'bakery', 'airport',
+buildings = ['cathedral', 'supermarket', 'cafe', 'shrine', 'apartment building', 'bakery', 'airport',
              'fire department', 'gas station', 'book store', 'bowling alley', 'church', 'mosque',
              'school building', 'library', 'temple', 'restaurant', 'museum', 'cinema', 'movie theater',
              'house', 'hospital', 'bus terminal', 'convenience store', 'shopping mall', 'office building',
-             'barber shop', 'bank', 'train station', 'department store', 'post office'}
+             'barber shop', 'bank', 'train station', 'department store', 'post office']
 
-clothes = {'belt', 'jumper', 'pajamas', 'shoes', 'pants', 'coat', 'jeans', 'overalls', 'trousers',
-           'overcoat', 'underpants', 'sweatshirt', 'skirt', 'blouse', 'shorts', 'slacks', 'boots',
+clothes = ['belt', 'jumper', 'pajamas', 'shoes', 'pants', 'jeans', 'overalls', 'trousers',
+           'overcoat', 'underpants', 'sweatshirt', 'skirt', 'blouse', 'shorts', 'slacks',
            't-shirt', 'hat', 'sweater', 'scarf', 'stockings', 'mini-skirt', 'pantyhose', 'cap',
-           'raincoat', 'cardigan', 'sweat-shirt', 'socks', 'tie', 'underclothes', 'gloves', 'vest',
-           'shirt', 'dress', 'jacket', 'slippers', 'suit', 'undershirt'}
+           'raincoat', 'cardigan', 'sweat-shirt', 'sock', 'tie', 'underclothes', 'gloves', 'vest',
+           'shirt', 'dress', 'jacket', 'suit', 'undershirt']
 
-fruit = {'banana', 'blueberry', 'prune', 'blackberry', 'persimmon', 'cranberry', 'melon', 'peach',
+fruit = ['banana', 'blueberry', 'prune', 'blackberry', 'persimmon', 'cranberry', 'melon', 'peach',
          'lemon', 'nectarine', 'pineapple', 'strawberry', 'lime', 'apple', 'fig', 'grapes',
          'cherry', 'pear', 'orange', 'plum', 'tangerine', 'currant', 'raspberry', 'apricot',
-         'watermelon', 'grape', 'kiwi', 'kumquat', 'grapefruit', 'pomegranate'}
+         'watermelon', 'grape', 'kiwi', 'kumquat', 'grapefruit', 'pomegranate']
 
-vegetables = {'sweet potato', 'carrot', 'okra', 'potato', 'eggplant', 'broccoli', 'beet', 'peas',
+vegetables = ['sweet potato', 'carrot', 'okra', 'potato', 'eggplant', 'broccoli', 'beet', 'peas',
               'kale', 'lettuce', 'Japanese radish', 'pumpkins', 'asparagus', 'tomatoes',
               'Brussels sprouts', 'onion', 'cauliflower', 'radish', 'spinach', 'Chinese cabbage', 'corn',
-              'cabbage', 'green pepper', 'cucumber', 'beans', 'celery'}
+              'cabbage', 'green pepper', 'cucumber', 'beans', 'celery']
 
 categories = {'animals:': animals, 'body parts': body_parts, 'buildings': buildings, 'clothes': clothes, 'fruit': fruit, 'vegetables': vegetables}
 
